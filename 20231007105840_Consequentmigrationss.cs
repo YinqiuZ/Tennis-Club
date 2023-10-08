0b4d22be04ecaa3495c7a550ce0434087e4e89f4
YinqiuZ <146711315+YinqiuZ@users.noreply.github.com>
2023-10-08 07:16:04 +0300

    migrations

---
+++ 20231007105840_Consequentmigrationss.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alpha3.Migrations
+{
+    /// <inheritdoc />
+    public partial class Consequentmigrationss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Bio",
+                table: "CoachProfiles",
+                newName: "Biography");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Schedules",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Schedules",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Schedules");
+
+            migrationBuilder.RenameColumn(
+                name: "Biography",
+                table: "CoachProfiles",
+                newName: "Bio");
+        }
+    }
+}
---
+++ 20231007113047_consecutivemig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alpha3.Migrations
+{
+    /// <inheritdoc />
+    public partial class consecutivemig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
